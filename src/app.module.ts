5ab63dd75c5be183ffb3b7582b4a39f008e97643
001osama <osamanasim909@gmail.com>
2023-10-05 18:07:38 +0500

    adding mongo and validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://threads-user:Threads123@cluster0.a2kbchu.mongodb.net/threads?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
