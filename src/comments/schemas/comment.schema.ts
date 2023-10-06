da97ef5c137d567f44a75a052d2c20f4258266c3
001osama <osamanasim909@gmail.com>
2023-10-06 22:23:09 +0500

    comment schema implemented

--- src/comments/schemas/comment.schema.ts
+++ src/comments/schemas/comment.schema.ts
@@ -1,15 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from 'src/users/schemas/user.schema';
 
 export type CommentDocument = HydratedDocument<Comment>;
 
-@Schema()
+@Schema({
+    timestamps:true
+  })
+  
 export class Comment {
   @Prop()
   text: string;
-  userId: string;
-  parentId: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User'})
+  user: User;
+  
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Comment'})
+  parent: Comment|null;
+  
+  @Prop()
   likes: number;
 }
 
